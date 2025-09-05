93b1e7f201f702801a3e7307d271f987af3f1653
tle19 <tyle2003@gmail.com>
2025-09-04 23:56:31 -0700

    fixed overnight hold logic

--- backtest.py
+++ backtest.py
@@ -15,14 +15,13 @@ def run_backtest(strategy, symbol, dh, date="2024-10-01", initial_cash=30_000):
     for _, row in df.iterrows():
         signal = strat.update(row)
         close = row['close']
-
+        print(signal)
         if signal == 1:
             cash -= close
             position = 1
         elif signal == -1:
             cash += close
             position = 0
-
         equity_curve.append(cash + position * close)
     
     df['equity'] = equity_curve
@@ -34,5 +33,48 @@ def run_statistics(strategy, df):
     if strategy == 'Momentum':
         HOD_probability(df)
         bearish_overnight_return(df)
-    if strategy == 'mean_reversal':
+    elif strategy == 'mean_reversal':
         return
+
+
+
+# def run_backtest(strategy, symbol, dh, start_date="2024-10-01", end_date=None, initial_cash=30_000):
+#     df = dh.open_data(symbol)
+
+#     if end_date is not None:
+#         df = df[(df.index.date >= pd.to_datetime(start_date).date()) &
+#                 (df.index.date <= pd.to_datetime(end_date).date())]
+#     else:
+#         df = df[df.index.date >= pd.to_datetime(start_date).date()]
+
+#     if df.empty:
+#         print("No data in the specified date range.")
+#         return
+    
+#     cash = initial_cash
+#     position = 0
+#     equity_curve = []
+
+#     run_statistics(strategy, df)
+#     strat = strategy()
+
+#     for date, day_group in df.groupby(df.index.date):
+#         for _, row in day_group.iterrows():
+#             signal = strat.update(row)
+#             close = row['close']
+
+#             if signal == 1 and position == 0:
+#                 cash -= close
+#                 position = 1
+#             elif signal == -1 and position == 1:
+#                 cash += close
+#                 position = 0
+
+#             equity_curve.append(cash + position * close)
+            
+#         print(date, " ", cash)
+#         print(close)
+
+#     df['equity'] = equity_curve
+#     summary(df, initial_cash)
+#     profits(df, symbol, date)
--- strategy/momentum.py
+++ strategy/momentum.py
@@ -1,17 +1,14 @@
+from datetime import time
+
 import pandas as pd
 
+
 class Momentum:
-    def __init__(self,
-                 hod_json="hod_results.json",
-                 overnight_json="overnight_results.json",
-                 profit_ratio=0.01,
-                 max_holding_days=1):
+    def __init__(self, hod_json="hod_results.json", overnight_json="overnight_results.json", profit_ratio=0.005):
         self.profit_ratio = profit_ratio
-        self.max_holding_days = max_holding_days
 
         self.in_position = False
         self.entry_price = 0
-        self.holding_days = 0
         self.trade_done_today = False
         self.bullish_trade_done = False
         self.bearish_trade_done = False
@@ -35,41 +32,37 @@ class Momentum:
             self.bullish_trade_done = False
             self.bearish_trade_done = False
             self.last_date = current_date
+        
 
         signal = 0
 
-        # ----------- Entry Logic -----------
-        if not self.bullish_trade_done and not self.in_position:
-            self.in_position = True
-            self.entry_price = close
-            self.holding_days = 0
-            self.bullish_trade_done = True
-            signal = 1
-
-        elif not self.bearish_trade_done and not self.in_position:
-            if low <= self.entry_price:
-                self.in_position = True
-                self.entry_price = low
-                self.holding_days = 0
-                self.bearish_trade_done = True
-                signal = 1
-
         # ----------- Exit Logic -----------
-        if self.in_position:
-            self.holding_days += 1
+        if self.in_position:  
             if close >= self.entry_price * (1 + self.profit_ratio):
                 signal = -1
                 self._exit_trade()
             elif close >= self.entry_price * (1 + self.avg_pct_return):
                 signal = -1
                 self._exit_trade()
-            elif self.holding_days >= self.max_holding_days:
+            elif row.name.time() == time(9, 30):
                 signal = -1
                 self._exit_trade()
-
+        else:
+        # ----------- Entry Logic -----------
+            if not self.bullish_trade_done:
+                self.in_position = True
+                self.entry_price = close
+                self.bullish_trade_done = True
+                signal = 1
+            elif not self.bearish_trade_done:
+                if low <= self.entry_price:
+                    self.in_position = True
+                    self.entry_price = low
+                    self.bearish_trade_done = True
+                    signal = 1
+        
         return signal
 
     def _exit_trade(self):
         self.in_position = False
         self.entry_price = 0
-        self.holding_days = 0
