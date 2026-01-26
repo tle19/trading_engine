469dca6d6424efd9a2b552774bf1fdc53f0bbcf0
tle19 <tyle2003@gmail.com>
2026-01-26 15:10:44 -0800

    made simple pair buy/sell

--- testing.py
+++ testing.py
@@ -17,15 +17,38 @@ def test_order(symbol="AAPL"):
     fill_price = eq.get_fill_price(exit_id, timeout=0.1)
     print(fill_price)
 
+def get_positions():
+    response = pt.client.account_details(pt.hash, fields="positions")
+    data = response.json()
+    positions = data.get('securitiesAccount', {}).get('positions', [])
+    return positions
 
 pt = Pairs(["GOOG", "GOOGL"], StochasticIndicator)
-# pt.run()
 
-def enter():
-    pt.buy_market("GOOG", 1, type="BUY")
-    pt.sell_market("GOOGL", 1, type="SELL_SHORT")
+def buy_pair(shares=100):
+    positions = get_positions()
+    if not positions:
+        pt.buy_market("GOOG", shares, type="BUY")
+        pt.sell_market("GOOGL", shares, type="SELL_SHORT")
+    else:
+        pt.buy_market("GOOG", shares, type="BUY_TO_COVER")
+        pt.sell_market("GOOGL", shares, type="SELL")
 
-def exit():
-    pt.sell_market("GOOG", 1, type="SELL")
-    pt.buy_market("GOOGL", 1, type="BUY_TO_COVER")
+def sell_pair(shares=100):
+    positions = get_positions()
+    if not positions:
+        pt.sell_market("GOOG", shares, type="SELL_SHORT")
+        pt.buy_market("GOOGL", shares, type="BUY")
+    else:
+        pt.sell_market("GOOG", shares, type="SELL")
+        pt.buy_market("GOOGL", shares, type="BUY_TO_COVER")
 
+for _ in range(60):
+    response = pt.client.quotes(["GOOG", "GOOGL"])
+    data = response.json()
+    goog_info = data.get("GOOG", {}).get("quote", {})
+    googl_info = data.get("GOOG", {}).get("quote", {})
+    goog_ask = goog_info['askPrice']
+    googl_ask = googl_info['askPrice']
+    print("GOOG", goog_ask, "GOOGL", googl_ask)
+    time.sleep(5)
